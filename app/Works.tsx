2b21733f476bd9c4e39cc8c56cca56bb2a6b7bff
Arsile <ajiprio@hotmail.com>
2023-02-03 22:09:49 +0700

    Animating works section

--- app/Works.tsx
+++ app/Works.tsx
@@ -1,6 +1,7 @@
 'use client';
-import {videos} from '../constants';
-import {motion} from 'framer-motion';
+import { videos } from '../constants';
+import { motion } from 'framer-motion';
+import { fadeIn, staggerContainer } from '../lib/motion';
 
 const Works = () => {
   return (
@@ -8,20 +9,29 @@ const Works = () => {
         <h1 className="text-[30px] mb-12 underline-blue font-bold uppercase">
             Works
         </h1>
-        <div className='grid sm:grid-rows-1 lg:grid-cols-3 max-w-7xl'>
-          {videos.map((video) => (
-            <motion.img 
-            whileHover={{ scale: 1.1 }}
-            whileTap={{scale: 1.0}}
-            key={video.key}
-            src={`https://img.youtube.com/vi/${video.videoId}/maxresdefault.jpg`}
-            alt=""
-            width="1280"
-            height="720"
-            className='-mt-3'
+        <motion.div 
+          className='grid border-none sm:grid-rows-1 lg:grid-cols-3 max-w-7xl'
+          variants={staggerContainer()}
+          initial="hidden"
+          whileInView="show"
+          viewport={{once: false, amount: 0.25}}
+        >
+          {videos.map((video, index) => (
+            <motion.img
+              initial="hidden"
+              whileInView="show"
+              variants={fadeIn('up', 'tween', index * 0.2, 1)}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{scale: 1.0}}
+              key={video.key}
+              src={`https://img.youtube.com/vi/${video.videoId}/maxresdefault.jpg`}
+              alt=""
+              width="1280"
+              height="720"
+              className=''
             />
           ))}
-        </div>
+        </motion.div>
 
         {/* <div className="flex">
           {videos.map((video) =>(
