09439734278c34a40d89c964871d7b9581b64991
Arsile <ajiprio@hotmail.com>
2023-02-03 22:17:53 +0700

    Added reels animation

--- app/Reels.tsx
+++ app/Reels.tsx
@@ -1,13 +1,20 @@
 'use client'
+import {motion} from 'framer-motion'
 import YouTubeLazyLoad from "../components/YouTubeLazyLoad"
+import { fadeIn } from '../lib/motion'
 
 
 function Reels() {
   return (
-    <div className="flex justify-center items-center -mt-24">
+    <motion.div 
+      variants={fadeIn('up', 'tween', 0.25, 0.5)}
+      initial="hidden"
+      whileInView="show"
+      className="flex justify-center items-center -mt-24"
+    >
       <YouTubeLazyLoad youtubeId="jagncTkYq_Y"/>
       {/* <div className="gradient-1  "/> */}
-    </div>
+    </motion.div>
   )
 }
 export default Reels
