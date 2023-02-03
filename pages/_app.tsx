c46f6573f205dea04e983331a0ad1d7c50fab0a7
Arsile <ajiprio@hotmail.com>
2023-02-03 22:25:20 +0700

    Fixed analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { MantineProvider } from '@mantine/core'
-import { Analytics } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
